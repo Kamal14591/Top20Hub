9231eafe927342dd98e6b2cc5823dc2d58753b6d
Kamal Kishor Nayak <kamal.kknayak@gmail.com>
2017-07-31 00:43:13 +0530

    Intial Commit (Setup UI project)

---
+++ top20hub-Backend/src/main/java/com/kj/top20hub_Backend/App.java
@@ -0,0 +1,13 @@
+package com.kj.top20hub_Backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
